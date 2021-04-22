8481c005e3672772e1f12a16005a56965f6baf29
Florents Tselai <florents.tselai@gmail.com>
2021-04-23 00:17:33 +0300

    Fix connection

--- src/main/java/store/DataCatalogStore.java
+++ src/main/java/store/DataCatalogStore.java
@@ -3,6 +3,22 @@ package store;
 import core.WorkerTaskResult;
 import core.config.ProfilerConfig;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class DataCatalogStore implements Store {
@@ -27,28 +43,119 @@ public class DataCatalogStore implements Store {
         return false;
     }
 
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:" + System.getProperty("user.home") + "/catalog.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
     @Override
     public boolean storeDocument(WorkerTaskResult wtr) {
-        /*
-        long id,
-        String dbName,
-        String path,
-        String sourceName,
-        String columnName,
-        String dataType,
-        int totalValues,
-        int uniqueValues,
-        String entities,
-        long[] minhash
-         */
+        String strId = Long.toString(wtr.getId());
 
-        System.out.println(wtr.getSourceName());
-        System.out.println(wtr.getColumnName());
-        System.out.println(wtr.getDataType());
+        XContentBuilder builder = null;
+        try {
+            builder = jsonBuilder()
+                    .startObject()
+                    .field("item_id", wtr.getId())
+                    .field("what_profile_id", wtr.getId())
+                    .field("asset_id", wtr.getPath() + '.' + wtr.getColumnName())
+                    .startObject("what_profile_schema")
+                    .field("path", wtr.getPath())
+                    .field("sourceName", wtr.getSourceName())
+                    .field("columnNameNA", wtr.getColumnName())
+                    .field("columnName", wtr.getColumnName())
+                    .field("dataType", wtr.getDataType())
+                    .field("totalValues", wtr.getTotalValues())
+                    .field("uniqueValues", wtr.getUniqueValues())
+                    .field("entities", wtr.getEntities().toString())
+                    .startArray("minhash");
+
+            long[] mh = wtr.getMH();
+            if (mh != null) { // that's an integer column
+                for (long i : mh) {
+                    builder.value(i);
+                }
+            } else {
+                builder.value(-1);
+            }
+
+            builder.endArray()
+
+                    .field("minValue", wtr.getMinValue())
+                    .field("maxValue", wtr.getMaxValue())
+                    .field("avgValue", wtr.getAvgValue())
+                    .field("median", wtr.getMedian())
+                    .field("iqr", wtr.getIQR())
+                    .endObject()
+                    .endObject();
+
+            String jsonPayload = builder.string();
+            //System.out.println(builder.string());
+
+            // WRITE DIRECTLY TO CATALOG DBs
+
+            String sql = "INSERT INTO what_profile(item_id, what_profile_id, version, timestamp, asset_id, what_profile_schema, user_item_id) " +
+                    "VALUES(?,?,?,?,?,?,?)";
+
+            try (Connection conn = this.connect();
+                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, wtr.toString());
+                pstmt.setString(2, wtr.toString());
+                pstmt.setString(3, "0.1");
+                pstmt.setString(4, "");
+                pstmt.setString(5, wtr.getPath() + '.' + wtr.getColumnName());
+                pstmt.setString(6, jsonPayload);
+                pstmt.setString(7, null);
 
 
-        System.out.println("========");
-        return false;
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//            // WRITE VIA HTTP
+//            URL url = new URL(this.storeUrl + "/profile/what");
+//            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//            con.setRequestMethod("POST");
+//            con.setRequestProperty("Content-Type", "application/json; utf-8");
+//            con.setRequestProperty("Accept", "application/json");
+//            con.setDoOutput(true);
+//            try (OutputStream os = con.getOutputStream()) {
+//                byte[] input = jsonPayload.getBytes("utf-8");
+//                os.write(input, 0, input.length);
+//            }
+//
+//            try (BufferedReader br = new BufferedReader(
+//                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+//                StringBuilder response = new StringBuilder();
+//                String responseLine = null;
+//                while ((responseLine = br.readLine()) != null) {
+//                    response.append(responseLine.trim());
+//                }
+//                System.out.println(response.toString());
+//            }
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+        return true;
     }
 
     @Override
